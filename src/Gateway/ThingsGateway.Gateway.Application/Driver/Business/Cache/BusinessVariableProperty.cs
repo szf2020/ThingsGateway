2078b4a60b43569236dac83be83d64001a4d6ec1
2248356998 qq.com <2248356998@qq.com>
2025-08-26 08:54:09 +0800

    补充忽略的文件

---
+++ src/Gateway/ThingsGateway.Gateway.Application/Driver/Business/Cache/BusinessVariableProperty.cs
@@ -0,0 +1,37 @@
+﻿//------------------------------------------------------------------------------
+//  此代码版权声明为全文件覆盖，如有原作者特别声明，会在下方手动补充
+//  此代码版权（除特别声明外的代码）归作者本人Diego所有
+//  源代码使用协议遵循本仓库的开源协议及附加协议
+//  Gitee源代码仓库：https://gitee.com/diego2098/ThingsGateway
+//  Github源代码仓库：https://github.com/kimdiego2098/ThingsGateway
+//  使用文档：https://thingsgateway.cn/
+//  QQ群：605534569
+//------------------------------------------------------------------------------
+
+namespace ThingsGateway.Gateway.Application;
+/// <summary>
+/// <inheritdoc/>
+/// </summary>
+public class BusinessVariableProperty : VariablePropertyBase
+{
+    [DynamicProperty]
+    public string Data1 { get; set; }
+    [DynamicProperty]
+    public string Data2 { get; set; }
+    [DynamicProperty]
+    public string Data3 { get; set; }
+    [DynamicProperty]
+    public string Data4 { get; set; }
+    [DynamicProperty]
+    public string Data5 { get; set; }
+    [DynamicProperty]
+    public string Data6 { get; set; }
+    [DynamicProperty]
+    public string Data7 { get; set; }
+    [DynamicProperty]
+    public string Data8 { get; set; }
+    [DynamicProperty]
+    public string Data9 { get; set; }
+    [DynamicProperty]
+    public string Data10 { get; set; }
+}
