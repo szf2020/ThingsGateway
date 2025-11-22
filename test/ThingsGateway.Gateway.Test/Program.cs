4f5178c478b701334b7893b4d368e8a4d6ef85d2
2248356998 qq.com <2248356998@qq.com>
2025-11-22 13:38:07 +0800

    build: 12.4.6
    fix: opcua json编解码错误
    feat: excel导入可空类型判断

---
+++ test/ThingsGateway.Gateway.Test/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json.Nodes;
+
+using ThingsGateway.Foundation.OpcUa;
+
+namespace ThingsGateway.Gateway.Test
+{
+    internal sealed class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            OpcUaMaster opcUaMaster = new();
+            OpcUaProperty opcUaProperty = new();
+            opcUaProperty.OpcUrl = "opc.tcp://localhost:49321";
+
+            opcUaMaster.OpcUaProperty = opcUaProperty;
+
+            await opcUaMaster.ConnectAsync(default).ConfigureAwait(false);
+
+            var read = await opcUaMaster.WriteNodeAsync( new Dictionary<string, JsonNode>()
+            {
+                {"ns=2;s=modbusDevice743916562268229.modbus41",21 }, 
+                {"ns=2;s=modbusDevice743916562268232.modbus41",12 },
+            }).ConfigureAwait(false);
+
+            Console.WriteLine("Hello World!");
+
+            Console.ReadLine();
+        }
+    }
+}
